1297de9fc258e66bafe315f7ecf7515bf3015f39
Serg <benderlio@gmail.com>
2023-04-23 07:46:46 +0200

    return object

--- nodejs-version/api.js
+++ nodejs-version/api.js
@@ -1,26 +1,64 @@
+
 import http from 'http';
-import url from 'url';
 import getElevation from './getElevation.js';
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer(async (req, res) => {
-    const queryObject = url.parse(req.url, true).query;
+const server = http.createServer(async (request, response) => {
 
-    const lat = queryObject.lat || '';
-    const lng = queryObject.lng || '';
-
-    const elevation = await getElevation(+lat, +lng)
-
-    const result = {
-        lat,
-        lng,
-        elevation: elevation || null
+    if (request.method == 'POST') {
+        var body = ''
+        request.on('data', function (data) {
+            body += data
+        })
+        request.on('end', async function () {
+            const coordinates = JSON.parse(body);
+            const elevation = await getElevation(coordinates)
+            const result = {
+                elevation: elevation || null
+            }
+            response.writeHead(200, { 'Content-Type': 'text/html' })
+            response.write(JSON.stringify(result))
+            response.end('')
+        })
+    } else {
+        const result = {
+            "data": "Only POST requests are allowed"
+        }
+        response.writeHead(200, { 'Content-Type': 'text/html' })
+        response.write(JSON.stringify(result))
+        response.end()
     }
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end(JSON.stringify(result));
+    /*
+
+    let body = `[
+        [-47.66755677759648,-22.832906062124344 ],
+        [-47.66725677759648,-22.832906062124344 ],
+        [-47.66215677759648,-22.832906062124344 ]
+    ]
+    `;
+    req.on('data', chunk => {
+        body += JSON.parse(chunk.toString());
+    });
+
+    req.on('end', async () => {
+        const coordinates = JSON.parse(body);
+        console.log('Received POST array:', coordinates);
+        const elevation = await getElevation(coordinates)
+        console.log('elevation--->', elevation);
+
+        const result = {
+            elevation: elevation.map(d => d[0]) || null
+        }
+
+        console.log('re', result);
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.write(JSON.stringify(result));
+        res.end();
+    });
+    */
+
 });
 
 server.listen(port, hostname, () => {
--- nodejs-version/getElevation.js
+++ nodejs-version/getElevation.js
@@ -3,7 +3,7 @@ import proj4 from 'proj4';
 import fs from 'fs';
 
 
-async function getElevation(lat, lng) {
+async function getElevation(coordsArray) {
     // Define the source and target coordinate systems
     const src = proj4.defs('EPSG:4326'); // Geographic coordinates (latitude and longitude)
     const dst = proj4.defs('EPSG:4326'); // Web Mercator projection
@@ -13,22 +13,28 @@ async function getElevation(lat, lng) {
     const tiff = await fromArrayBuffer(arrayBuffer)
     const image = await tiff.getImage();
     const metadata = await image.getFileDirectory();
-    // const { lng, lat } = { lat: -22.832906062124344, lng: -47.66755677759648 }
-    // Convert the latitude and longitude coordinates to projected coordinates
-    const [x, y] = proj4(src, dst, [lng, lat]);
-    // Calculate the pixel coordinates corresponding to the projected coordinates
-    const [pixelX, pixelY] = [
-        Math.round((x - metadata.ModelTiepoint[3]) / metadata.ModelPixelScale[0]),
-        Math.round((metadata.ModelTiepoint[4] - y) / metadata.ModelPixelScale[1])
-    ];
-    // Get the elevation data at the pixel coordinates
-    const data = await image.readRasters({
-        window: [pixelX, pixelY, pixelX + 1, pixelY + 1],
-        interleave: true
-    });
-    // Print the elevation data
-    console.log(data[0]);
-    return data[0]
+
+    const promises = coordsArray.map(c => {
+        const [x, y] = proj4(src, dst, [c.lat, c.lng]);
+        // Calculate the pixel coordinates corresponding to the projected coordinates
+        const [pixelX, pixelY] = [
+            Math.round((x - metadata.ModelTiepoint[3]) / metadata.ModelPixelScale[0]),
+            Math.round((metadata.ModelTiepoint[4] - y) / metadata.ModelPixelScale[1])
+        ];
+        // Get the elevation data at the pixel coordinates
+        return image.readRasters({
+            window: [pixelX, pixelY, pixelX + 1, pixelY + 1],
+            interleave: true
+        }).then(res => ({
+            lat: c.lat,
+            lon: c.lng,
+            res: res[0]
+        }))
+    })
+
+    const res = await Promise.all(promises)
+
+    return res
 }
 
 export default getElevation;
